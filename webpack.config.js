f557b8b3b07c660fa97b945a1a2bd7746d0b15eb
Torey Littlefield <ToreyLIttlefield@eignited.com>
2022-04-02 23:15:29 +0800

    feat: webpack w/ GASPlugin & TerserPlugin

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const GasPlugin = require('gas-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const config = {
+  entry: './src/index.ts',
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: false,
+          output: {
+            comments: /@customFunction/i,
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [new GasPlugin()],
+};
+
+module.exports = config;
