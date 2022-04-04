2623af9c0af554501d75fc960516ae76a0f6e2f6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-04-04 19:11:57 +0800

    refactor: getDraft and setDraft, add sendEmail func

--- src/email/email.ts
+++ src/email/email.ts
@@ -1,4 +1,5 @@
 import { getProps, setInitialEmailProps, setUserProps } from '../properties-service/properties-service';
+import { EMAIL_ACCOUNT, NAME_TO_SEND_IN_EMAIL } from '../variables';
 
 export function setTemplateMsg({ subject, email }: { subject: string; email: string }) {
   const drafts = GmailApp.getDrafts();
@@ -10,16 +11,31 @@ export function setTemplateMsg({ subject, email }: { subject: string; email: str
   });
 }
 
-export function getDraft() {
+export function setDraftTemplateAutoResponder() {
   setInitialEmailProps();
   const props = getProps(['subject', 'email', 'draftId', 'messageId']);
-  console.log({ props });
   let { subject, email, draftId, messageId } = props;
   if (!draftId || !messageId) {
     setTemplateMsg({ subject, email });
-    const draftObj = getProps(['draftId']);
-    draftId = draftObj.draftId;
   }
+}
+
+function getDraftTemplateAutoResponder() {
+  const { draftId } = getProps(['draftId']);
   const draft = GmailApp.getDraft(draftId);
   return draft;
+}
+
+export function sendTemplateEmail(recipient: string, subject: string) {
+  try {
+    const draft = getDraftTemplateAutoResponder();
+    if (!draft) throw Error('Could not find draft and send Email');
+    GmailApp.sendEmail(recipient, subject, '', {
+      from: EMAIL_ACCOUNT,
+      htmlBody: draft.getMessage().getBody(),
+      name: NAME_TO_SEND_IN_EMAIL,
+    });
+  } catch (error) {
+    console.error(error as any);
+  }
 }
