e6b45a9e044c2162b7e16f9cba566016b01ec087
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-04-04 16:45:28 +0800

    feat: store userprops like local storage

---
+++ src/properties-service/properties-service.d.ts
@@ -0,0 +1,3 @@
+export declare function setUserProps(props: Record<string, any>): void;
+export declare function getProps(keys: string[]): Record<string, any>;
+export declare function setInitialEmailProps(): void;
---
+++ src/properties-service/properties-service.ts
@@ -0,0 +1,25 @@
+import { CANNED_MSG_NAME, EMAIL_ACCOUNT } from '../variables';
+
+export function setUserProps(props: Record<string, any>) {
+  const userProps = PropertiesService.getUserProperties();
+
+  userProps.setProperties(props);
+}
+
+export function getProps(keys: string[]) {
+  const userProps = PropertiesService.getUserProperties();
+  const props: Record<string, any> = {};
+  keys.forEach((key) => {
+    const value = userProps.getProperty(key);
+    props[key] = value;
+  });
+  return props;
+}
+
+export function setInitialEmailProps() {
+  const userProps = PropertiesService.getUserProperties();
+
+  if (!userProps.getProperty('subject') || !userProps.getProperty('email')) {
+    setUserProps({ subject: CANNED_MSG_NAME, email: EMAIL_ACCOUNT });
+  }
+}
