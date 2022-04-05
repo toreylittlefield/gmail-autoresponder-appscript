44050ed98e0197cd316217a25f1a81a53c4730b3
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-04-05 09:53:50 +0800

    feat: add additional sheets for do not reply and formatting

--- src/sheets/sheets.ts
+++ src/sheets/sheets.ts
@@ -2,12 +2,23 @@ import { getProps, setUserProps } from '../properties-service/properties-service
 import {
   AUTOMATED_SHEET_HEADERS,
   AUTOMATED_SHEET_NAME,
+  BOUNCED_SHEET_NAME,
+  BOUNCED_SHEET_NAME_HEADERS,
+  DO_NOT_EMAIL_AUTO_INITIAL_DATA,
+  DO_NOT_EMAIL_AUTO_SHEET_HEADERS,
+  DO_NOT_EMAIL_AUTO_SHEET_NAME,
   SENT_SHEET_NAME,
   SENT_SHEET_NAME_HEADERS,
   SPREADSHEET_NAME,
 } from '../variables/publicvariables';
 
-const tabColors = ['blue', 'green', 'purple'];
+const tabColors = ['blue', 'green', 'red', 'purple'];
+
+type SheetNames =
+  | typeof AUTOMATED_SHEET_NAME
+  | typeof SENT_SHEET_NAME
+  | typeof BOUNCED_SHEET_NAME
+  | typeof DO_NOT_EMAIL_AUTO_SHEET_NAME;
 
 function checkExistsOrCreateSpreadsheet() {
   let { spreadsheetId } = getSpreadSheetId();
@@ -19,6 +30,7 @@ function checkExistsOrCreateSpreadsheet() {
       PropertiesService.getUserProperties().deleteProperty('spreadsheetId');
       PropertiesService.getUserProperties().deleteProperty('sheetId');
       PropertiesService.getUserProperties().deleteProperty('sheetName');
+      PropertiesService.getUserProperties().deleteAllProperties();
       spreadsheetId = null;
     }
   }
@@ -30,14 +42,16 @@ function checkExistsOrCreateSpreadsheet() {
       spreadsheetId: spreadsheet.getId(),
     });
     createSheet(spreadsheet, AUTOMATED_SHEET_NAME, AUTOMATED_SHEET_HEADERS, 'blue');
-    createSheet(spreadsheet, SENT_SHEET_NAME, SENT_SHEET_NAME_HEADERS, 'green');
     spreadsheet.deleteSheet(firstSheet);
+    createSheet(spreadsheet, SENT_SHEET_NAME, SENT_SHEET_NAME_HEADERS, 'green');
+    createSheet(spreadsheet, DO_NOT_EMAIL_AUTO_SHEET_NAME, DO_NOT_EMAIL_AUTO_SHEET_HEADERS, 'purple');
+    createSheet(spreadsheet, BOUNCED_SHEET_NAME, BOUNCED_SHEET_NAME_HEADERS, 'red');
   }
 }
 
 function createSheet(
   activeSS: GoogleAppsScript.Spreadsheet.Spreadsheet,
-  sheetName: string,
+  sheetName: SheetNames,
   headersValues: string[],
   tabColor?: string
 ) {
@@ -74,7 +88,7 @@ function setSheetInProps(sheetName: string, activeSheet: GoogleAppsScript.Spread
   setUserProps({ [sheetName]: activeSheet.getSheetId().toString() });
 }
 
-function getSheet(activeSS: GoogleAppsScript.Spreadsheet.Spreadsheet, sheetName: string) {
+function getSheet(activeSS: GoogleAppsScript.Spreadsheet.Spreadsheet, sheetName: SheetNames) {
   try {
     const [sheetNameProp, sheetIdProp] = Object.entries(getProps([sheetName]))[0];
 
@@ -93,7 +107,7 @@ function getSheet(activeSS: GoogleAppsScript.Spreadsheet.Spreadsheet, sheetName:
   }
 }
 
-function getActiveSheet(activeSS: GoogleAppsScript.Spreadsheet.Spreadsheet, sheetName: string) {
+function getActiveSheet(activeSS: GoogleAppsScript.Spreadsheet.Spreadsheet, sheetName: SheetNames) {
   try {
     const sheet = getSheet(activeSS, sheetName);
     if (sheet) {
@@ -128,12 +142,38 @@ function setSheetProtection(activeSheet: GoogleAppsScript.Spreadsheet.Sheet, des
   }
 }
 
+function setInitialDoNotReplyData(activeSS: GoogleAppsScript.Spreadsheet.Spreadsheet) {
+  try {
+    const doNotEmailSheet = getActiveSheet(activeSS, DO_NOT_EMAIL_AUTO_SHEET_NAME);
+    if (!doNotEmailSheet) throw Error('Cannot Find Do Not Email Sheet');
+    const initData = doNotEmailSheet
+      .getRange(2, 2, DO_NOT_EMAIL_AUTO_INITIAL_DATA.length, DO_NOT_EMAIL_AUTO_INITIAL_DATA.length)
+      .getValues();
+    const hasInitialData = initData.every((row, index) => row[index] === DO_NOT_EMAIL_AUTO_INITIAL_DATA[index]);
+    if (!hasInitialData) {
+      DO_NOT_EMAIL_AUTO_INITIAL_DATA.forEach((row) => {
+        doNotEmailSheet.appendRow(row);
+      });
+    }
+  } catch (error) {
+    console.error(error as any);
+  }
+}
+
+export function formatRowHeight(activeSS: GoogleAppsScript.Spreadsheet.Spreadsheet) {
+  const automatedSheet = getActiveSheet(activeSS, 'Automated Results List');
+  automatedSheet && automatedSheet.setRowHeights(2, automatedSheet.getDataRange().getNumRows(), 21);
+  const sentResponsesSheet = getActiveSheet(activeSS, 'Sent Automated Responses');
+  sentResponsesSheet && sentResponsesSheet.setRowHeights(2, sentResponsesSheet.getDataRange().getNumRows(), 21);
+}
+
 export function initSpreadsheet() {
   checkExistsOrCreateSpreadsheet();
 
   setActiveSpreadSheet(SpreadsheetApp);
 
   const activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  setInitialDoNotReplyData(activeSpreadsheet);
   const activeSheet = getActiveSheet(activeSpreadsheet, AUTOMATED_SHEET_NAME);
 
   if (!activeSheet) return;
