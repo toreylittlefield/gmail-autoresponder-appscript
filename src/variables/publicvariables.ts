55b9ce1b2f215fdde5131a4b08afeff86671b38e
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-04-06 13:59:32 +0800

    update sheets with email thread id and add new sheet for follow ups list

--- src/variables/publicvariables.ts
+++ src/variables/publicvariables.ts
@@ -8,30 +8,76 @@ export const LABEL_NAME = 'hasAutoResponse';
 export const BOUNCED_MESSAGES_REGEX = new RegExp('mailer-daemon@googlemail.com|@bounce|@bounced', 'gi');
 
 /** Name of the spreadsheet to create */
-export const SPREADSHEET_NAME = 'Job Tracker';
+export const SPREADSHEET_NAME = 'Email Autoresponder - Job Tracker';
 
 /** Name of the sheet for the emails found results */
 export const AUTOMATED_SHEET_NAME = 'Automated Results List';
 export const AUTOMATED_SHEET_HEADERS = [
-  'Email Id',
+  'Email Thread Id',
+  'Email Message Id',
   'Date',
   'From',
   'ReplyTo',
+  'Person / Company Name',
   'Subject',
   'Body Emails',
   'Body',
   'Salary',
   'Email Permalink',
+  'Thread Permalink',
   'Has Email Response',
 ];
 
 /** Name of the sheet for the sent auto response email data */
 export const SENT_SHEET_NAME = 'Sent Automated Responses';
-export const SENT_SHEET_NAME_HEADERS = ['Email Id', 'Date', 'From', 'ReplyTo', 'Subject', 'Body', 'Email Permalink'];
+export const SENT_SHEET_NAME_HEADERS = [
+  'Email Thread Id',
+  'Sent Email Message Id',
+  'In Response To Email Message Id',
+  'Date',
+  'From',
+  'ReplyTo',
+  'Person / Company Name',
+  'Subject',
+  'Body',
+  'Email Permalink',
+  'Thread Permalink',
+];
+
+/** Name of the sheet for the emails found results */
+export const FOLLOW_UP_EMAILS_SHEET = 'Follow Up Emails Received List';
+export const FOLLOW_UP_EMAILS_HEADERS = [
+  'From Domain',
+  'Email Thread Id',
+  'Email Message Id',
+  'Date',
+  'From',
+  'ReplyTo',
+  'Person / Company Name',
+  'Subject',
+  'Body Emails',
+  'Body',
+  'Salary',
+  'Email Permalink',
+  'Thread Permalink',
+  'Has Email Response',
+];
 
 /** Name of the sheet for the any bounced emails */
 export const BOUNCED_SHEET_NAME = 'Bounced Responses';
-export const BOUNCED_SHEET_NAME_HEADERS = ['Email Id', 'Date', 'From', 'ReplyTo', 'Subject', 'Body', 'Email Permalink'];
+export const BOUNCED_SHEET_NAME_HEADERS = [
+  'Email Thread Id',
+  'Sent Email Message Id',
+  'In Response Email Message Id',
+  'Date',
+  'From',
+  'Person / Company Name',
+  'ReplyTo',
+  'Subject',
+  'Body',
+  'Email Permalink',
+  'Thread Permalink',
+];
 
 /** Name of the sheet for the any bounced emails */
 export const DO_NOT_TRACK_DOMAIN_LIST_SHEET_NAME = 'Do Not Track List';
