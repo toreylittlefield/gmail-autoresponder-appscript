668a577a043db1c9f407747c91e3a1941bb9c7d9
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-04-16 20:15:44 +0800

    add: delete all existing triggers function

--- src/trigger/trigger.ts
+++ src/trigger/trigger.ts
@@ -4,15 +4,22 @@ import { getEmailsFromGmail } from '../index';
  *
  * Check if trigger already exists for a function
  */
-function hasTriggerByName(functionName: Function['name']) {
-  return ScriptApp.getProjectTriggers().some((trigger) => trigger.getHandlerFunction() === functionName);
+function hasTriggerByName(nameOfFunction: Function['name']) {
+  return ScriptApp.getProjectTriggers().some((trigger) => trigger.getHandlerFunction() === nameOfFunction);
 }
 
 /**
- * Creates time-driven trigger for {@link getEmailsFromGmail} that runs every 1 hour
+ * Creates time-driven trigger for {@link getEmailsFromGmail} that runs every 1 hour if it does not already exist
  * @see https://developers.google.com/apps-script/guides/triggers/installable#time-driven_triggers
  */
 export function createTriggerForEmailsSync() {
-  if (!hasTriggerByName(getEmailsFromGmail.name))
+  if (!hasTriggerByName(getEmailsFromGmail.name)) {
     ScriptApp.newTrigger(getEmailsFromGmail.name).timeBased().everyHours(1);
+    return true;
+  }
+  return false;
+}
+
+export function deleteAllExistingProjectTriggers() {
+  ScriptApp.getProjectTriggers().forEach((trigger) => ScriptApp.deleteTrigger(trigger));
 }
